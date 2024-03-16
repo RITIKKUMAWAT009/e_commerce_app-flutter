8d10cb354e332b462b57363c704300a3f65de0f5
RITIKKUMAWAT009 <ritikkumawat140@gmail.com>
2024-03-16 21:31:55 +0530

    image and text constants file added

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,6 @@
+class TImages{
+
+  //App logos
+  static const String google="assets/logos/google_icon.png";
+  static const String facebook="assets/logos/facebook_icon.png";
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,20 @@
+class TTexts{
+  TTexts._();
+  //Global Text
+
+
+  //onBoarding Text
+  static const String onBoardingTitle1="Choose your product";
+  static const String onBoardingTitle2="Select Payment Methode";
+  static const String onBoardingTitle3="Deliver at your door step";
+
+  static const String onBoardingSubTitle1="Welcome to a World of limitless Choices - Your perfect Product Awaits!";
+  static const String onBoardingSubTitle2="For Seamless Transactions, Choose Your Payment Path - Your Conversation, Our Priority!";
+  static const String onBoardingSubTitle3="From Our Doorstep to your - Swift, Secure, and Contactless Delivery!";
+
+  //Home
+static const String homeAppBarTitle='Good day for shopping';
+static const String homeAppBarSubTitle='Ritik kumawat';
+
+
+}
