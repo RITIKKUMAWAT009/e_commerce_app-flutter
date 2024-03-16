9fc6e7b94741053984d835c772a5bbf2c7d6a96e
RITIKKUMAWAT009 <ritikkumawat140@gmail.com>
2024-03-17 01:15:47 +0530

    validator,logger and storage files are added

---
+++ lib/utils/local_storage/storage_utility.dart
@@ -0,0 +1,24 @@
+import 'package:get_storage/get_storage.dart';
+class TLocalStorage{
+ static final TLocalStorage _instance=TLocalStorage._internal();
+  factory TLocalStorage(){
+    return _instance;
+  }
+  TLocalStorage._internal();
+  final _storage=GetStorage();
+  //generic methode to save data
+Future<void> saveData<T>(String key,T value)async{
+  await _storage.write(key, value);
+}
+//read data
+T? readData<T>(String key){
+  return _storage.read<T>(key);
+}
+//remove data
+Future<void> removeData(String key)async{
+  await _storage.remove(key);
+}
+Future<void>clearAll()async{
+  await _storage.erase();
+}
+}
