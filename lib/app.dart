2a4fbf87a508780778b565b17d6cba8fd6729feb
RITIKKUMAWAT009 <ritikkumawat140@gmail.com>
2024-03-18 23:19:58 +0530

    Register screen completed

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_app/features/authentication/screens/login/login.dart';
 import 'package:e_commerce_app/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:e_commerce_app/features/authentication/screens/signup/signup.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:e_commerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +15,7 @@ themeMode: ThemeMode.system,
       darkTheme: TAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       theme: TAppTheme.lightTheme,
+      // home: const SignUpScreen(),
       home:const OnBoardingScreen(),
       // home:const LoginScreen(),
     );
---
+++ lib/features/authentication/screens/signup/signup.dart
@@ -0,0 +1,50 @@
+import 'package:e_commerce_app/common/widgets_login_signup/form_divider.dart';
+import 'package:e_commerce_app/common/widgets_login_signup/social_button.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/constants/text_strings.dart';
+import 'package:e_commerce_app/utils/helper/helper_function.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'widgets/signup_form.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = THelperFunction.isDarkMode(context);
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: Theme.of(context).iconTheme,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:const EdgeInsets.all(TSize.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //title
+              Text(
+                TTexts.signUpTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(
+                height: TSize.spaceBtwSections,
+              ),
+              //form
+             const SignUpForm(),
+              //divider
+              const SizedBox(height: TSize.spaceBtwSections,),
+              FormDivider(dividerText: TTexts.orSignUpWith.capitalize!),
+              //social button
+              const SizedBox(height: TSize.spaceBtwSections,),
+             const SocialButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
