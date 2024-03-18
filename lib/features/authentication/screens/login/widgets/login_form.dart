a676ce924fe1600783fc9b8d2696dd426d0cb2af
RITIKKUMAWAT009 <ritikkumawat140@gmail.com>
2024-03-18 21:37:52 +0530

    login screen completed

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,9 +1,6 @@
 
-import 'package:e_commerce_app/features/authentication/screens/signup/signup.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-
 import '../../../../../utils/constants/colors.dart';
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/constants/text_strings.dart';
@@ -82,9 +79,7 @@ class LoginForm extends StatelessWidget {
                 width: double.infinity,
                 child: ElevatedButton(style:isDark? const ButtonStyle(backgroundColor: MaterialStatePropertyAll(TColors.dark)):const ButtonStyle(backgroundColor: MaterialStatePropertyAll(TColors.light)
                 ),
-                  onPressed: () =>
-                   Get.to(()=>
-    const SignUpScreen(),),
+                  onPressed: () {},
                   child:  Text(TTexts.createAccount, style: Theme.of(context).textTheme.bodyMedium,),
                 )),
           ],
